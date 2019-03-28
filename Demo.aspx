c66b3558390ae44d51faa6308c0f6c62e9789ab9
Saranya993K <49018396+Saranya993K@users.noreply.github.com>
2019-03-28 18:24:15 +0530

    Demo pages
    Basic implementation of javascript validations and combo boxes

---
+++ Demo.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Demo.aspx.cs" Inherits="WebApp.Demo" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+           
+            <asp:DropDownList ID="Country" runat="server" OnSelectedIndexChanged="Country_SelectedIndexChanged" AutoPostBack="true"></asp:DropDownList>
+            <asp:DropDownList ID="States" runat="server" Visible="false" ></asp:DropDownList>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Demo.aspx.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApp
+{
+    public partial class Demo : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                List<ListItem> country = new List<ListItem>();
+                country.Add(new ListItem("Select Country", "Select"));
+                country.Add(new ListItem("India", "India"));
+                country.Add(new ListItem("USA", "USA"));
+                country.Add(new ListItem("China", "China"));
+
+                //country.Sort(delegate (ListItem item1, ListItem item2) { return item1.Text.CompareTo(item2.Text); });
+                Country.Items.AddRange(country.ToArray());
+            }
+        }
+        protected void AddItem(object sender, EventArgs e)
+        { }
+
+        protected void Country_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            States.Visible = true;
+            var item = Country.SelectedValue;
+            List<ListItem> states = new List<ListItem>();
+            
+            switch (item)
+            {
+                case "India":
+                    states.Add(new ListItem("Maharastra","1"));
+                    states.Add(new ListItem("TamilNadu", "2"));
+                    states.Add(new ListItem("Andhrapradesh", "3"));
+                    break;
+                case "USA":
+                    states.Add(new ListItem("Canada", "1"));
+                    states.Add(new ListItem("California", "2"));
+                    states.Add(new ListItem("Florida", "3"));
+                    break;
+                case "China":
+                    states.Add(new ListItem("Shandong", "1"));
+                    states.Add(new ListItem("Yunnan", "2"));
+                    states.Add(new ListItem("Fujian", "3"));
+                    break;
+                default:
+                    states.Add(new ListItem("No Items", "1"));
+                    break;                   
+            }
+            States.Items.Clear();
+            //States.DataSource = states.ToArray();
+            States.Items.AddRange(states.ToArray());
+        }
+    }
+}
