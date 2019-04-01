6590a4e5fd4ea71b65ffb47b65765afe99d9a642
Saranya993K <49018396+Saranya993K@users.noreply.github.com>
2019-04-02 00:25:50 +0530

    Sample application with MVC

---
+++ EmployeeLoginController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using DBAccess;
+
+namespace TestAPIApp.Controllers
+{
+    public class EmployeeLoginController : ApiController
+    {
+        [HttpGet]
+        public bool GetLogins(string username,string password)
+        {
+            using (SampleDBEntities ent = new SampleDBEntities())
+            {
+                return Validate(username, password);
+            }
+        }
+
+       // [HttpGet]
+        public bool Validate(string username, string password)
+        {
+            bool IsAvaiable = false;
+            using (SampleDBEntities ent = new SampleDBEntities())
+            {
+                IsAvaiable = ent.EmployeeLogins.Where(e => e.UserName.Equals(username) && e.Password.Equals(password)).Any();   
+            }
+            return IsAvaiable;
+        }
+    }
+}
