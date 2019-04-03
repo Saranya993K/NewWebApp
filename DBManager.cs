432106cd2b0f39cd6d8e8735a3e11b07288fe9c0
Saranya993K <49018396+Saranya993K@users.noreply.github.com>
2019-04-03 19:40:11 +0530

    Changes included

---
+++ DBManager.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using TestAPIApp.Models;
+
+namespace TestAPIApp.Controllers
+{
+    public class DBManager
+    {
+        //SqlConnection con = new SqlConnection();
+        private static string conString = ConfigurationManager.ConnectionStrings["Connstring"].ConnectionString;
+        //public void GetConnection()
+        //{
+        //    string conString = ConfigurationManager.ConnectionStrings["Connstring"].ConnectionString;
+        //    con = new SqlConnection(conString);
+
+        //    #region NoNeed
+        //    //try
+        //    //{
+        //    //string conString = ConfigurationManager.ConnectionStrings["Connstring"].ConnectionString;
+        //    //con = new SqlConnection(conString);
+        //    //con.Open();
+        //    //}
+        //    //catch(Exception e)
+        //    //{
+        //    //}
+        //    //finally
+        //    //{
+        //    //    con.Close();
+        //    //}
+        //    #endregion
+        //}
+
+        public List<State> GetStates(string CountryID)
+        {
+            List<State> sList = new List<State>();            
+            SqlConnection con = new SqlConnection();
+            try
+            {
+                using (con = new SqlConnection(conString))
+                {
+                    string query = "SELECT StateID,StateName from States where CountryID=" + CountryID;
+                    using (SqlCommand cmd = new SqlCommand(query))
+                    {
+                        cmd.Connection = con;
+                        con.Open();
+                        using (SqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                State c = new State { StateID = Convert.ToInt32(reader["StateID"]), StateName = reader["StateName"].ToString() };
+                                sList.Add(c);
+                            }
+                        }
+                    }
+                }
+                return sList;
+            }
+            catch(Exception)
+            {
+                return null;
+            }
+            finally
+            {
+                if (con != null)
+                { con.Close(); }
+            }
+        }
+
+
+        public List<Country> GetCountries()
+        {
+            List<Country> countries = new List<Country>();
+            SqlConnection con = new SqlConnection();
+            try
+            {
+                using (con = new SqlConnection(conString))
+                {
+                    string query = "SELECT CountryID, CountryName FROM Countries";
+                    using (SqlCommand cmd = new SqlCommand(query))
+                    {
+                        cmd.Connection = con;
+                        con.Open();
+                        using (SqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                Country c = new Country { CountryID = Convert.ToInt32(reader["CountryID"]), CountryName = reader["CountryName"].ToString() };
+                                countries.Add(c);
+                            }
+                        }
+                    }
+                }
+                return countries;
+            }
+            catch(Exception)
+            {
+                return null;
+            }
+            finally
+            {
+                if (con != null)
+                { con.Close(); }
+            }
+        }
+
+        public bool ValidateLogin(string username, string password)
+        {
+            SqlConnection con = new SqlConnection();
+            try
+            {                
+                using (con = new SqlConnection(conString))
+                {
+                    string query = "SELECT Count(*) As cnt FROM EmployeeLogin WHERE Username='" + username + "' AND Password='" + password + "'";
+                    using (SqlCommand cmd = new SqlCommand(query))
+                    {
+                        cmd.Connection = con;
+                        con.Open();
+
+                        int count = Convert.ToInt32(cmd.ExecuteScalar());
+
+                        if (count > 0)
+                            return true;   
+                    }
+                }
+            }
+            catch(Exception)
+            {                
+            }
+            finally
+            {
+                if (con != null)
+                { con.Close(); }
+            }
+            return false;
+        }
+    }
+}
--- EmployeeLoginController.cs
+++ EmployeeLoginController.cs
@@ -1,33 +1,147 @@
-﻿using System;
+﻿//using DBAccess;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
-using DBAccess;
+using System.Web.Mvc;
+//using DBAccess;
+using TestAPIApp.Models;
 
 namespace TestAPIApp.Controllers
 {
     public class EmployeeLoginController : ApiController
     {
-        [HttpGet]
-        public bool GetLogins(string username,string password)
+        [System.Web.Http.HttpGet, System.Web.Http.Route("api/Login")]
+        public bool GetValidaate(string username,string password)
         {
-            using (SampleDBEntities ent = new SampleDBEntities())
+            #region anotherwayForConnectionEstablishment
+            //SqlConnection conn = new SqlConnection();
+            //conn.ConnectionString =
+            //"Data Source=DESKTOP-EM068UD;" +
+            //"Initial Catalog=MyDB;" +
+            //"Integrated Security=SSPI;";
+            //conn.Open();
+            #endregion
+
+            DBManager db = new DBManager();
+            return db.ValidateLogin(username, password);
+
+            #region oldStuff
+
+            //string constr = ConfigurationManager.ConnectionStrings["Connstring"].ConnectionString;
+            //using (SqlConnection con = new SqlConnection(constr))
+            //{
+            //    string query = "SELECT Count(*) As cnt FROM EmployeeLogin WHERE Username='"+username+"' AND Password='"+password+"'";                
+            //    using (SqlCommand cmd = new SqlCommand(query))
+            //    {
+            //        cmd.Connection = con;
+            //        cmd.CommandType = System.Data.CommandType.Text;
+            //        con.Open();                  
+
+            //        int count = Convert.ToInt32(cmd.ExecuteScalar());
+            //        con.Close();
+            //        if (count > 0)
+            //            return true;
+            //        else
+            //            return false;                   
+            //    }
+            //}           
+            #endregion
+        }
+        [System.Web.Http.HttpGet, System.Web.Http.Route("api/GetCountries")]
+        public List<Country> GetCountries()
+        {
+            try
             {
-                return Validate(username, password);
+                DBManager db = new DBManager();
+                List<Country> countries = new List<Country>();
+                return countries = db.GetCountries();
             }
+            catch
+            {   throw;  }
+
+            #region oldStuff
+            //string constr = ConfigurationManager.ConnectionStrings["Connstring"].ConnectionString;
+            //using (SqlConnection con = new SqlConnection(constr))
+            //{
+            //    string query = "SELECT CountryID, CountryName FROM Countries";
+            //    using (SqlCommand cmd = new SqlCommand(query))
+            //    {
+            //        cmd.Connection = con;                   
+            //        con.Open();
+            //        SqlDataReader reader = cmd.ExecuteReader();                    
+            //        while (reader.Read())
+            //        {
+            //            Country c = new Country { CountryID = Convert.ToInt32(reader["CountryID"]), CountryName = reader["CountryName"].ToString() };
+            //            countries.Add(c);
+            //        }
+            //    }
+            //    return countries;
+            //}
+            #endregion
         }
 
-       // [HttpGet]
-        public bool Validate(string username, string password)
+        [System.Web.Http.HttpGet, System.Web.Http.Route("api/GetStates")]
+        public List<State> GetStateByCountry(string CountryID)
         {
-            bool IsAvaiable = false;
-            using (SampleDBEntities ent = new SampleDBEntities())
+            try
             {
-                IsAvaiable = ent.EmployeeLogins.Where(e => e.UserName.Equals(username) && e.Password.Equals(password)).Any();   
+                List<State> States = new List<State>();
+                DBManager db = new DBManager();
+                var returnList = db.GetStates(CountryID);
+                if (returnList != null)
+                {
+                    States = returnList;
+                }
+                return States;
             }
-            return IsAvaiable;
+            catch(Exception)
+            { return null; }
+
+            #region oldStuff
+            //string constr = ConfigurationManager.ConnectionStrings["Connstring"].ConnectionString;
+            //using (SqlConnection con = new SqlConnection(constr))
+            //{
+            //    string query = "SELECT StateID,StateName from States where CountryID="+ CountryID;
+            //    using (SqlCommand cmd = new SqlCommand(query))
+            //    {
+            //        cmd.Connection = con;                    
+            //        con.Open();
+            //        SqlDataReader reader = cmd.ExecuteReader();                    
+            //        while (reader.Read())
+            //        {
+            //            State s = new State { StateID = Convert.ToInt32(reader["StateID"]), StateName = reader["StateName"].ToString() };
+            //            States.Add(s);
+            //        }
+            //    }
+            //    return States;
+            //}
+            #endregion
         }
+
+        //[System.Web.Http.HttpGet, System.Web.Http.Route("api/GetLogins")]
+        //public bool GetLogins(string username, string password)
+        //{
+        //    using (SampleDBEntities ent = new SampleDBEntities())
+        //    {
+        //        return Validate(username, password);
+        //    }
+        //}
+
+        //// [HttpGet]
+        //public bool Validate(string username, string password)
+        //{
+        //    bool IsAvaiable = false;
+        //    using (SampleDBEntities ent = new SampleDBEntities())
+        //    {
+        //        IsAvaiable = ent.EmployeeLogins.Where(e => e.UserName.Equals(username) && e.Password.Equals(password)).Any();
+        //    }
+        //    return IsAvaiable;
+        //}
     }
 }
